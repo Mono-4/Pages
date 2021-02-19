70dcb50c39e0153fb2b9606d155de61e3c8aafb9
Yusui Hoshino <linkwater99@yahoo.ne.jp>
2021-02-19 23:17:26 +0900

    sort code

--- MakeWallGame/main.js
+++ MakeWallGame/main.js
@@ -1,27 +1,25 @@
 "use strict";
+//準備--------------------------------------------------------
 let n = 4;
 let total_n = n * 2 + 1;
 let mass_long = 100;
 let mass_short = 20;
 let stage = 0;
 let count_stage = 0;
+// クリックされた場所
 let ci = 0;
 let cj = 0;
 
 const p1 = document.querySelector(".p1");
 const p2 = document.querySelector(".p2");
 const h2 = document.querySelector("h2");
-const judge_result = document.querySelector(".result");
+// ステップの各要素
 const li = [];
 li[0] = document.querySelector("ul li:nth-child(1)");
 li[1] = document.querySelector("ul li:nth-child(2)");
 li[2] = document.querySelector("ul li:nth-child(3)");
 li[3] = document.querySelector("ul li:nth-child(4)");
-
-document.querySelector(".input1").addEventListener("click", () => {
-  window.location.reload();
-});
-
+// プレイヤーのいち
 let posi = [
   {
     i: 1,
@@ -37,9 +35,8 @@ var a = new Array(total_n);
 for (var i = 0; i < total_n; i++) {
   a[i] = new Array(total_n).fill(0);
 }
-//一つだけクリックされた状態を作りたい
-//マスを長さを指定しながら作成
-//　EventListenerも作成
+// tdを作成、配置。クラスを割り振る
+//EventListenerを定義
 for (let i = 0; i < total_n; i++) {
   const tr = document.createElement("tr");
   for (let j = 0; j < total_n; j++) {
@@ -65,12 +62,9 @@ for (let i = 0; i < total_n; i++) {
       }
     }
 
-    //--
     a[i][j] = td;
     td.addEventListener("click", () => {
-      //クリックされた位置を特定
       [ci, cj] = [td.parentNode.rowIndex, td.cellIndex];
-      //プレイヤークラス全解除
       remove_player_class();
       click();
       add_player_class();
@@ -80,13 +74,17 @@ for (let i = 0; i < total_n; i++) {
   }
   document.querySelector(".field").appendChild(tr);
 }
-
+// ボタンをクリックしたらリロードでリスタート
+document.querySelector(".input1").addEventListener("click", () => {
+  window.location.reload();
+});
+//下準備
 add_player_class();
 update_text();
 mark_mass(0);
 
-//MAIN FUNTION-----------------------------------------------------------------------------------------------------------
-
+//Click Functions------------------------------------------------
+//クリックされたら、状態によって実行する関数を割り振る
 function click() {
   switch (stage) {
     case 0:
@@ -115,7 +113,7 @@ function click() {
 
       break;
 
-    case 2: //Aが３会　Aが３会移動
+    case 2:
       if (move_check(1)) {
         mark_remove();
         mark_mass(1);
@@ -130,17 +128,18 @@ function click() {
       }
       break;
 
-    case 3: //Aが　壁作る
+    case 3:
       if (make_wall()) {
         stage = 0;
         mark_remove();
         mark_mass(0);
-        if (judge() >= 0) stage = 100; //判定　何も無ければreturn 0 勝敗決まれば stage=100で離脱
+        //勝敗が付いたら、離脱
+        if (judge() >= 0) stage = 100;
       }
       break;
   }
 }
-//FUNCTIONS------------------------------------------------------------------------
+//MARK FUNCTIONS------------------------------------------------------------------------
 function mark_mass(p) {
   var i = posi[p].i;
   var j = posi[p].j;
@@ -178,8 +177,8 @@ function remove_player_class() {
     }
   }
 }
-
-//posiから現在のプライヤーのいちを特定、クラスを割り振る
+//FUNCTIONS------------------------------------------------------------------------
+// プレイヤーの場所を表示
 function add_player_class() {
   if (posi[0].i === posi[1].i && posi[0].j === posi[1].j) {
     a[posi[1].i][posi[1].j].classList.add("p3");
@@ -188,7 +187,7 @@ function add_player_class() {
     a[posi[1].i][posi[1].j].classList.add("p2");
   }
 }
-
+// 上のテキストを表示
 function update_text() {
   var s;
   let s1 = `move ${count_stage}/3`;
@@ -260,21 +259,18 @@ function update_text() {
   }
 }
 
-//勝敗が付いていたらstage==100で終了
+//プレイヤーの勝敗を調べる
 function judge() {
   var x = 0;
   var y = 0;
-  //囲まれていたら1
+
   if (are_there_4wall(0)) x = 1;
   if (are_there_4wall(1)) y = 1;
-  //勝敗つかない　引き分け　blueの勝ち　redの勝ち
+
   if (x === 0 && y === 0) return 0;
   else if (x === 1 && y === 1) return 1;
-  //s_result = "DRAW";
   else if (x === 1) return 2;
-  //s_result = "BLUE win";
-  else return 2; //s_result = "RED win";
-  //勝敗がついたのでstageを0-4から離脱　結果を表示
+  else return 2;
 }
 //周りが全てwallなら1を返す
 function are_there_4wall(p) {
@@ -287,9 +283,9 @@ function are_there_4wall(p) {
     a[i][j - 1].classList.contains("wall")
   ) {
     return 1;
-  } else return 0; //動けるかラナにもしない
+  } else return 0;
 }
-//wasteでなく、壁でなく、massでない=壁が建てる
+//壁を建てる
 function make_wall() {
   if (a[ci][cj].classList.contains("nowall")) {
     a[ci][cj].classList.remove("nowall");
@@ -298,14 +294,12 @@ function make_wall() {
   }
   return 0;
 }
-//移動できるなら1を返す
-//壁がなく、その方向に進みたい時。
+// 移動できるかチェック＞関数を呼び出す
 function move_check(p) {
   var i = posi[p].i;
   var j = posi[p].j;
   //選択がマスじゃない
   if (!a[ci][cj].classList.contains("mass")) return 0;
-  //そのさきがクリックされた　しかも壁がない＝動かす
   if (i - 2 === ci && j === cj) {
     if (a[i - 1][j].classList.contains("nowall")) {
       move(p, i - 2, j);
@@ -332,12 +326,12 @@ function move_check(p) {
   }
   return 0;
 }
-//posiを更新
+//プレイヤーを移動
 function move(p, i, j) {
   posi[p].i = i;
   posi[p].j = j;
 }
-//-----------------------
+//Explain FUNCTIONS-----------------------
 {
   const menuItems = document.querySelectorAll(".menu li a");
   const contents = document.querySelectorAll(".content");
